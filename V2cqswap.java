0e078d7a8f47b426715569c2c68ca25655d0f555
untermischling <omegachad333@proton.me>
2026-07-31 10:30:25 -0300

    Create V2cqswap.java
    Improved code for cqswap (copied from svart)

---
+++ V2cqswap.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class V2cqswap{
+  public static void main(String[] args)throws IOException{
+    String suffix = " Reacto to 'School Shooting' for the First Time";
+    Files.lines(Paths.get(denonyms)).forEach(denonym -> System.out.println(denonym + suffix));
+  }
+}
