dba2435dbd80095b2bbf6b1cd8350fdc53ce4e5a
untermischling <omegachad333@proton.me>
2026-07-31 10:32:57 -0300

    Update V2cqswap.java
    fixed path error.

--- V2cqswap.java
+++ V2cqswap.java
@@ -5,6 +5,6 @@ import java.nio.file.Paths;
 public class V2cqswap{
   public static void main(String[] args)throws IOException{
     String suffix = " Reacto to 'School Shooting' for the First Time";
-    Files.lines(Paths.get(denonyms)).forEach(denonym -> System.out.println(denonym + suffix));
+    Files.lines(Paths.get(denonyms.txt)).forEach(denonym -> System.out.println(denonym + suffix));
   }
 }
